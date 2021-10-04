7b60397114029d0cc2e7ac813a7451eb91ce1871
Denisa Marcisovska <dmarcisovska@gmail.com>
2021-10-04 14:41:33 -0700

    added counter buttons

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Loading from './Components/Loading'
 import Dogs from './Components/Dogs'
 import AmountDogs from './Components/AmountDogs'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 
-const url = 'https://dog.ceo/api/breeds/image/random/6'
+const url = 'https://dog.ceo/api/breeds/image/random/1'
 
 
 function App() {
 
   const [loading, setLoading] = useState(true);
   const [dogs, setDogs]= useState([]);
-  const [number, setNumber] = useState("");
+  const [number, setNumber] = useState(6);
+  const [counter, setCounter] = useState(6);
+  const incrementCounter = () => setNumber(number + 1);
+  const decrementCounter = () => setNumber(number - 1);
 
-//request specific amount of random dogs
+  if(counter<=0) {
+    decrementCounter = () => setCounter(1);
+  }
+
 useEffect(() => {
     setLoading(true)
-    const getNumberDogsDogs = async (number) => {
+    const getNumberDogs = async (number) => {
       const response = await fetch(`https://dog.ceo/api/breeds/image/random/${number}`);
       const data = await response.json();
       setDogs(data.message);
@@ -29,26 +36,26 @@ useEffect(() => {
       setLoading(false);
       return;
     };
-    getNumberDogsDogs(number);
+    getNumberDogs(number);
   }, [number]);
 
-  //request 6 random dogs 
-  useEffect(() => {
-    const getAllDogs = async () => {
-      const response = await fetch(url);
-      const data = await response.json();
-      setDogs(data.message);
-    }
-    getAllDogs();
-  }, []);
+ 
 
   return (
 
     <div className="App">
       <h1>Random Dogs</h1>
-      <AmountDogs number={setNumber} />
+    
+
+      <div className='dog-buttons'>
+        <Button variant="dark" className='btn-dog' onClick={()=> setNumber(number + 1)}>1 more dog</Button>
+        <Button variant="dark" className='btn-dog'  onClick={()=> setNumber(number - 1)}>1 less dog</Button>
+        <Button variant="dark" className='btn-dog'  onClick={()=> setNumber(number + 5)}>5 more dogs</Button>
+        <Button variant="dark" className='btn-dog'  onClick={()=> setNumber(number - 5)}>5 less dogs</Button>
+      </div>
+     
       {
-        loading ? <Loading /> : <Dogs dogs={dogs} />
+        loading ? <Loading /> : <Dogs dogs={dogs}  /> 
       }
     </div>
   );
--- src/Components/AmountDogs.js
+++ src/Components/AmountDogs.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const AmountDogs = ({number}) => {
 
+    
+
+
+    const handleClick = () => {
+        let number = 6;
+        number ++;
+        // console.log("counter", number);
+        alert(number)
+    };
+
     const handleChange = (event) => {
     number(event.target.value);
+    alert(event.target.value)
     }
 
     return (
         <div>
+            <Button variant="secondary"  onClick={handleClick} className='dogDropDown'>Increment by 6</Button>
+       
             <Form.Select aria-label="Default select example" onChange={handleChange} className="dogDropDown btn-dark">
                 <option>Chose amount of dogs</option>
                 <option value={6}>Six</option>
